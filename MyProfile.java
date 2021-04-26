6069a137f32b86560edbbc0cf940d786e919a325
newbie coder #42069 <40706567+debed@users.noreply.github.com>
2021-04-26 14:40:52 +0800

    Add files via upload

---
+++ MyProfile.java
@@ -0,0 +1,12 @@
+public class MyProfile {
+	public static void main(String[] args) {
+
+    String name = "David";
+    int age = 18;
+    double desiredSalary = 69;
+    char gender = 'm';
+    boolean lookingForJob = false;
+    System.out.println("Yep. Works.");
+
+	}
+}
